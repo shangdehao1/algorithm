d5fda7cff0589fd440584547eb6f241986e92984
shangdehao1 <34433535@qq.com>
2018-07-11 19:43:27 +0800

    695: max area of island---depth search

---
+++ leetcode/695.cc
@@ -0,0 +1,77 @@
+/*
+ 
+ Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
+ You may assume all four edges of the grid are surrounded by water.
+ 
+ Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+ 
+ Example 1:
+
+ [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+ Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+
+ Example 2:
+
+ [[0,0,0,0,0,0,0,0]]
+
+ Given the above grid, return 0.
+
+ Note: The length of each dimension in the given grid does not exceed 50.*
+
+ */
+
+
+
+// depth search method
+
+void dfs(vector<vector<int>>& grid, int x_index, int y_index, int& results) {
+    if(x_index < 0 || x_index > (grid.size()-1)) { // Note: bound
+        return;
+    }
+
+    if( y_index < 0 || y_index > (grid[0].size()-1)) {
+        return;
+    }
+
+    if(grid[x_index][y_index] == 2) {
+        return;
+    }
+
+    if(grid[x_index][y_index] == 0) {
+        grid[x_index][y_index] = 2;
+        return;
+    }
+
+    if(grid[x_index][y_index] == 1) {
+        results++;
+        grid[x_index][y_index] = 2;
+    }
+    dfs(grid, x_index - 1, y_index, results);
+    dfs(grid, x_index + 1, y_index, results);
+    dfs(grid, x_index, y_index-1, results);
+    dfs(grid, x_index, y_index + 1, results);
+}
+
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int results = 0;
+    for(int i = 0; i < grid.size(); i++) {
+        for(int j = 0; j < grid[0].size() ; j++) {
+            int temp = 0;
+            if(grid[i][j] == 1) {
+                dfs(grid, i, j, temp);
+            }
+            if(temp > results) {
+                results = temp;
+            }
+        }
+    }
+    return results;
+}
